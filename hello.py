8d8273a51b12334b15c55854dc546568cc0e9049
Breanna Baltaxe-Admony <bbaltaxe@ucsc.edu>
2019-06-27 16:57:40 -0700

    hosting html files

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+def index(): 
+    return 'welcome to a webpage'
+
+@app.route('/hello')
 def hello(): 
     return 'Hey its bre'
+
+@app.route('/map')
+def map(): 
+    return render_template('map.html')
+
+if __name__ == '__main__':
+    app.run()
