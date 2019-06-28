c5902e7cdd5ad5ccc4f9bf8105f397ed8f816869
EC2 Default User <ec2-user@ip-172-31-20-95.us-west-2.compute.internal>
2019-06-28 19:11:22 +0000

    working on incorporating map functions

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 from flask import Flask, url_for, render_template
 import json
 import yaml
+from joy import gen_map as gm
+import os.path
+
 
 app = Flask(__name__)
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+CSV = os.path.join(DIR,'../UCSC_Sound_Map.csv')
 with open('secrets.yaml','r') as api_file:
 	gkey = yaml.load(api_file)['api_keys']['google']
 
@@ -17,10 +23,11 @@ def hello():
     return 'Hey its bre'
 
 #making a map page
-@app.route('/map')
+@app.route('/joy/map')
 def map():
 	#generate map here, and then send it to a file named /templates/map.html
-	return render_template('map.html')
+	maploc = os.path.join(DIR,gm.generate_map(CSV,gkey))
+	return render_template(maploc)
 
 #json endpoint 
 @app.route('/authors')
