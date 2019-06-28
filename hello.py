75717b2183c2cdcd751bcdc3754440f3e9908fb8
EC2 Default User <ec2-user@ip-172-31-20-95.us-west-2.compute.internal>
2019-06-28 19:24:56 +0000

    integrating joy map

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
 from flask import Flask, url_for, render_template
 import json
 import yaml
+import os.path
+from joy import gen_map as gm
+
 
 app = Flask(__name__)
 with open('secrets.yaml','r') as api_file:
 	gkey = yaml.load(api_file)['api_keys']['google']
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+CSV = os.path.join(DIR,'../UCSC_Sound_Map.csv')
 
 
 #making several pages
@@ -17,10 +23,11 @@ def hello():
     return 'Hey its bre'
 
 #making a map page
-@app.route('/map')
+@app.route('/joy/map')
 def map():
 	#generate map here, and then send it to a file named /templates/map.html
-	return render_template('map.html')
+	maploc = os.path.join(DIR,gm.generate_map(CSV,gkey))
+	return render_template(maploc)
 
 #json endpoint 
 @app.route('/authors')
