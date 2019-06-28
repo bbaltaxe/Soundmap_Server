3a42fac2164c561fee470209a14826b30fd23193
EC2 Default User <ec2-user@ip-172-31-20-95.us-west-2.compute.internal>
2019-06-28 17:43:38 +0000

    ready for intern commits

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 from flask import Flask, url_for, render_template
 import json
+import yaml
 
 app = Flask(__name__)
+with open('secrets.yaml','r') as api_file:
+	gkey = yaml.load(api_file)['api_keys']['google']
+
 
 #making several pages
 @app.route('/')
@@ -15,8 +19,8 @@ def hello():
 #making a map page
 @app.route('/map')
 def map():
-    #generate map here, and then send it to a file named /templates/map.html
-    return render_template('map.html')
+	#generate map here, and then send it to a file named /templates/map.html
+	return render_template('map.html')
 
 #json endpoint 
 @app.route('/authors')
