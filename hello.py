1e9bc054a9e61a8f24d8dd6d992375a6ec23bae9
Breanna Baltaxe-Admony <bbaltaxe@ucsc.edu>
2019-06-27 17:09:06 -0700

    ready to add to ec2 for interns

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask, url_for, render_template
+import json
 
 app = Flask(__name__)
 
+#making several pages
 @app.route('/')
 def index(): 
     return 'welcome to a webpage'
@@ -10,9 +12,17 @@ def index():
 def hello(): 
     return 'Hey its bre'
 
+#making a map page
 @app.route('/map')
-def map(): 
+def map():
+    #generate map here, and then send it to a file named /templates/map.html
     return render_template('map.html')
 
+#json endpoint 
+@app.route('/authors')
+def authors():
+    madeby = ['bre', 'davin', 'joy']
+    return json.dumps(madeby)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
