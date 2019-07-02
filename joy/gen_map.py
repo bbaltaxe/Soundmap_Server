c040a2d0eadfc571658ce5a9fa48a9b6ae58c898
happyjoytotheworld <21964836+happyjoytotheworld@users.noreply.github.com>
2019-07-02 09:38:36 -0700

    Added new gen_map.py
    Adapted gen_map.py to work pulling data from a spreadsheet, hence changed input values to work with link for spreadsheet. see comments for more information about each function.

--- joy/gen_map.py
+++ joy/gen_map.py
@@ -1,13 +1,30 @@
-import gmplot as gp
-import pandas as pd
+#gen_map.py. contains functions to generate map from 
+# csv data, and are callable for data changes.
 
-def read_csv(name):
-    df = pd.read_csv(filepath_or_buffer = name, header = 1, usecols = ["Latitude", "Longitude"])
+import requests
+import os
+import io
+import pandas as pd
+import gmplot as gp
+
+#function using pandas to read information from csv into dataframe
+#input: none, use saved link of spreadsheets
+#returns the dataframe to be used for map generation
+def read_csv():
+    r = requests.get('https://docs.google.com/spreadsheets/d/1_4v6PQ3asgZ-5I5bI8jObU2bmccdgmW_gA-m_99hzss/export?format=csv&id=1_4v6PQ3asgZ-5I5bI8jObU2bmccdgmW_gA-m_99hzss')
+    data = r.text
+    buffer = io.StringIO(data)
+    df = pd.read_csv(filepath_or_buffer = buffer , header = 1, usecols = ["Latitude", "Longitude"])
     df = df.dropna()
     return df
 
-def generate_map(csv_name, api_key):
-    df = read_csv(csv_name)
+
+#uses gmplot to plot map
+#input: string api key for google maps
+# after drawing the map, it returns the name of the map html file
+# to be added to the flask webpage
+def generate_map(api_key):
+    df = read_csv()
     map1 = gp.GoogleMapPlotter(36.993600, -122.059900, 14) 
     map1.apikey = api_key
     lon = tuple(df.Longitude)
@@ -16,6 +33,6 @@ def generate_map(csv_name, api_key):
     for i in range(len(lat)):
         map1.marker(lat[i], lon[i], 'red')
     html_loc = "templates/joy_map.html"
-    mapname = 'joy_map.html'
     map1.draw(html_loc)
-    return mapname
+    return html_loc
+
