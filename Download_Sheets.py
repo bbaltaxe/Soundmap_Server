a43a9cf9f8365df9f056eb056ebeabb1448a357d
davinn19 <52328911+davinn19@users.noreply.github.com>
2019-07-02 09:47:43 -0700

    Update Download_Sheets.py

--- Download_Sheets.py
+++ Download_Sheets.py
@@ -2,4 +2,4 @@
 import pygsheets
 gc = pygsheets.authorize(service_file='Davin_credentials.json')
 file = gc.open_by_key("1_4v6PQ3asgZ-5I5bI8jObU2bmccdgmW_gA-m_99hzss")
-file.export(filename = file.title)
+file.export(filename = "UCSC_Sound_Map.csv")
