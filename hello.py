bcf43d285518c2ac2eb15ed3a157edeb12d71ca8
breanna <bbaltaxe@ucsc.edu>
2019-07-02 15:01:34 +0000

    included Joy's html file as the index page

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import json
 import yaml
 import os.path
 from joy import gen_map as jgm
-from Davin import gen_map as dgm
+#from Davin import gen_map as dgm
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ CSV = os.path.join(DIR,'../UCSC_Sound_Map.csv')
 #making several pages
 @app.route('/')
 def index(): 
-    return 'welcome to a webpage'
+    return render_template('instruct.html')
 
 @app.route('/hello')
 def hello(): 
@@ -30,9 +30,9 @@ def hello():
 def jmap():
 	return render_template(jgm.generate_map(CSV,gkey))
 
-@app.route('/davin/map')
-def dmap(): 
-	return render_template(dgm.generate_map(CSV,gkey))
+#@app.route('/davin/map')
+#def dmap(): 
+#	return render_template(dgm.generate_map(CSV,gkey))
 
 #json endpoint/ 
 @app.route('/authors')
